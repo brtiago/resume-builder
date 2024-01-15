4f94366be0e77dfae83d6b5d0e204c24f6d0466d
brtiago <tgribeiro@gmail.com>
2024-01-15 16:34:39 -0300

    update nas regras

--- venv/summary_builder.py
+++ venv/summary_builder.py
@@ -59,7 +59,8 @@ prompt_parts = [
 convo = model.start_chat(history=[
   {
     "role": "user",
-    "parts": "você é uma analista senior de RH. Escreva um sumario para curriculo com base nos requisitos de cargo que enviarei abaixo:"
+    # "parts": "você é uma analista senior de RH. Escreva um sumario para curriculo com base nos requisitos de cargo que enviarei abaixo:"
+    "parts": """você é uma analista senior de RH. Ao escrever as experiências profissionais use a técnica de redação de curriculo conhecida por fórmula x-y-z. Escreva um resumo para curriculo com base nos requisitos de cargo que enviarei abaixo:"""
   },
   {
     "role": "model",
@@ -67,6 +68,19 @@ convo = model.start_chat(history=[
   }
 ])
 
-convo.send_message("Analista Java Pleno")
+convo.send_message("Analista Java Junior")
+convo.send_message("""Desenvolvimento backend em Python (FastAPI e/ou Flask);
+Testes automatizados;
+Realizar a raspagem de dados;
+Criar funcionalidades e melhorias em sistemas já existes e novos sistemas;
+Pedir orientação e direcionar os problemas que estão além de suas habilidades;
+Não ter receio em alterar um código que você não escreveu
+Desenvolver melhorias e correções nos sistemas atuais;
+Disseminar boas práticas de desenvolvimento junto ao time de desenvolvimento.
+Formação Superior, sendo diferencial ligados à tecnologia;
+Bons conhecimentos práticos em Python;
+Experiência em banco de dados relacionais e construção de queries;
+Git;
+Conhecimento de padrões de arquitetura de softwares;""")
 
 print(convo.last.text)
