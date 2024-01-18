51178324a83181a2a8794ad8e0c390bb9d3dca91
brtiago <tgribeiro@gmail.com>
2024-01-18 16:17:28 -0300

    feat: melhorando o uso de palavras-chave

--- venv/creator.py
+++ venv/creator.py
@@ -1,5 +1,17 @@
-from resume_reader import read_file
+from file import read_file
+from resume import create_resumo
 
-resume = read_file(input("Digite o caminho completo do arquivo txt: "))
+# resume_txt = read_txtfile(input("Digite o caminho completo do arquivo txt: "))
+resume_pdf = read_file(input("Digite o caminho completo do arquivo pdf: "))
 
-print (resume)
+palavras_chave = input("Digite as palavras-chave a serem utilizadas para a criação do RESUMO PROFISSIONAL: ")
+palavras_chave = """desenvolvedor java, java EE, JSF 2, desenvolvimento e manutenção, SQL, dados oracle, tecnologias, 
+desenvolvimento de aplicações, MVC, utilizando, EJB, JSP, web, hibernate, spring, JPA, 
+desenvolvimento de sistemas, banco de dados """
+
+print(create_resumo(palavras_chave))
+
+
+
+print(resume_pdf)
+print(palavras_chave)
